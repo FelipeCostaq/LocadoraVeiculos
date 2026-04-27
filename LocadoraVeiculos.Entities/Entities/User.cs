adc7b0cf8ac765c59e38b5999cf5344b2c12487c
Felipe <felipecostasiqu@gmail.com>
2026-04-27 18:25:57 -0300

    feat(entitie): User model inheritance IdentityUser created

---
+++ LocadoraVeiculos.Entities/Entities/User.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Entities.Entities
+{
+    public class User : IdentityUser
+    {
+    }
+}

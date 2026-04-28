c33f1712b9f6e23cf98d900c95bb0d6b3e94d058
Felipe <felipecostasiqu@gmail.com>
2026-04-28 18:49:48 -0300

    feat(repository): Veiculo added

---
+++ LocadoraVeiculos.Infrastructure/Repositories/RepositoryVeiculo.cs
@@ -0,0 +1,35 @@
+﻿using LocadoraVeiculos.Entities.Entities;
+using LocadoraVeiculos.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Infrastructure.Repositories
+{
+    public class RepositoryVeiculo
+    {
+        private readonly DbContextOptions<LocadoraContext> _options;
+
+        public RepositoryVeiculo(DbContextOptions<LocadoraContext> options)
+        {
+            _options = options;
+        }
+
+        public async Task<List<Veiculo>> ListarVeiculos()
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                return await data.Veiculos.AsNoTracking().ToListAsync();
+            }
+        }
+
+        public async Task<List<Veiculo>> ListarVeiculosDisponiveis()
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                return await data.Veiculos.Where(v => v.Disponivel == true).ToListAsync();
+            }
+        }
+    }
+}

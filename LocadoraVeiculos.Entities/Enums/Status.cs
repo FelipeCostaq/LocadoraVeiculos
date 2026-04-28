5a8ee2a0c460ca4dcc8e9305abbcb68d21feeff5
Felipe <felipecostasiqu@gmail.com>
2026-04-28 16:10:45 -0300

    feat(enum): Status enum created

---
+++ LocadoraVeiculos.Entities/Enums/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Entities.Enums
+{
+    public enum Status
+    {
+        Ativa,
+        Concluída,
+        Cancelada
+    }
+}

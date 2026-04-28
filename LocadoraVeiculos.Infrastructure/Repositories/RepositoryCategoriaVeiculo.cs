a427387194dbd683e0b74e708a79257f7050460c
Felipe <felipecostasiqu@gmail.com>
2026-04-28 18:08:55 -0300

    feat(repository): CategoriaVeiculo and Cliente repositories added

---
+++ LocadoraVeiculos.Infrastructure/Repositories/RepositoryCategoriaVeiculo.cs
@@ -0,0 +1,27 @@
+﻿using LocadoraVeiculos.Entities.Entities;
+using LocadoraVeiculos.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Infrastructure.Repositories
+{
+    public class RepositoryCategoriaVeiculo
+    {
+        private readonly DbContextOptions<LocadoraContext> _options;
+
+        public RepositoryCategoriaVeiculo(DbContextOptions<LocadoraContext> options)
+        {
+            _options = options;
+        }
+
+        public async Task<List<CategoriaVeiculo>> ListarCategoriasVeiculos()
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                return await data.CategoriaVeiculo.ToListAsync();
+            }
+        }
+    }
+}
---
+++ LocadoraVeiculos.Infrastructure/Repositories/RepositoryCliente.cs
@@ -0,0 +1,35 @@
+﻿using LocadoraVeiculos.Entities.Entities;
+using LocadoraVeiculos.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Infrastructure.Repositories
+{
+    public class RepositoryCliente
+    {
+        private readonly DbContextOptions<LocadoraContext> _options;
+
+        public RepositoryCliente(DbContextOptions<LocadoraContext> options)
+        {
+            _options = options;
+        }
+
+        public async Task<List<Cliente>> ListarClientes(Cliente cliente)
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                return await data.Clientes.ToListAsync();
+            }
+        }
+
+        public async Task<Cliente?> ListarClientePorId(Guid id)
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                return await data.Clientes.FindAsync(id);
+            }
+        }
+    }
+}

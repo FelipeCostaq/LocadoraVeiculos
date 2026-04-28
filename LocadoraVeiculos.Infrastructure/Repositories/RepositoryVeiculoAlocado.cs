450b2fda691eb5c9de2950226b86485d06b2fdaa
Felipe <felipecostasiqu@gmail.com>
2026-04-28 18:50:08 -0300

    feat(repository): VeiculoAlocado added

---
+++ LocadoraVeiculos.Infrastructure/Repositories/RepositoryVeiculoAlocado.cs
@@ -0,0 +1,40 @@
+﻿using LocadoraVeiculos.Entities.Entities;
+using LocadoraVeiculos.Entities.Enums;
+using LocadoraVeiculos.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Infrastructure.Repositories
+{
+    public class RepositoryVeiculoAlocado
+    {
+        private readonly DbContextOptions<LocadoraContext> _options;
+
+        public RepositoryVeiculoAlocado(DbContextOptions<LocadoraContext> options)
+        {
+            _options = options;
+        }
+
+        public async Task<List<VeiculoAlocado>> ListarVeiculosAlocados()
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                return await data.VeiculosAlocados.AsNoTracking().ToListAsync();
+            }
+        }
+
+        public async Task<List<VeiculoAlocado>> ListarVeiculosAlocadosDisponibilidade()
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                return await data.VeiculosAlocados.Select(v => new VeiculoAlocado()
+                {
+                    PlacaVeiculo = v.PlacaVeiculo,
+                    Status = v.Status
+                }).AsNoTracking().ToListAsync();
+            }
+        }   
+    }
+}

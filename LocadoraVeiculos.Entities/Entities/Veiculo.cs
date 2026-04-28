8a4fda49c0a786b022a84f93632ff3e781bc0696
Felipe <felipecostasiqu@gmail.com>
2026-04-28 16:12:14 -0300

    feat(entity): Veiculo created

---
+++ LocadoraVeiculos.Entities/Entities/Veiculo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace LocadoraVeiculos.Entities.Entities
+{
+    public class Veiculo
+    {
+        [Key]
+        public string Placa { get; set; } = string.Empty;
+
+        [Required]
+        public string Marca { get; set; } = string.Empty;
+
+        [Required]
+        public string Modelo { get; set; } = string.Empty;
+
+        [Required]
+        public int Ano { get; set; }
+
+        [Required]
+        public string Cor { get; set; } = string.Empty;
+
+        [ForeignKey("CategoriaVeiculo")]
+        [Required]
+        public string CategoriaId { get; set; } = string.Empty;
+
+        public string? ImagemUrl { get; set; } = string.Empty;
+
+        public bool Disponivel { get; set; }
+
+        public bool Ativo { get; set; } = true;
+
+    }
+}

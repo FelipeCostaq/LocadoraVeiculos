6ce672467589d4bbb70e1edc9c92d6b938ea6b47
Felipe <felipecostasiqu@gmail.com>
2026-04-28 16:13:29 -0300

    feat(entity): CategoriaVeiculo created

---
+++ LocadoraVeiculos.Entities/Entities/CategoriaVeiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LocadoraVeiculos.Entities.Entities
+{
+    public class CategoriaVeiculo
+    {
+        // PK
+        public Guid Id { get; set; } = Guid.Empty;
+
+        [Required]
+        public string Nome { get; set; } = string.Empty;
+
+        public string? Descricao { get; set; } = string.Empty;
+
+        [Required]
+        public decimal ValorDiaria { get; set; }
+
+        public bool Ativo { get; set; } = true;
+    }
+}

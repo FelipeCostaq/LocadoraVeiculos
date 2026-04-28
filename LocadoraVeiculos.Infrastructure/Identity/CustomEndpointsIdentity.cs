4e6443fd8556e65c92861a375a6ae8be41059452
Felipe <felipecostasiqu@gmail.com>
2026-04-28 17:07:00 -0300

    feat(identity): identity config endpoints and swagger visibility swagger

---
+++ LocadoraVeiculos.Infrastructure/Identity/CustomEndpointsIdentity.cs
@@ -0,0 +1,28 @@
+﻿using LocadoraVeiculos.Entities.Entities;
+using LocadoraVeiculos.Infrastructure.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
+using System.Text.RegularExpressions;
+
+namespace LocadoraVeiculos.Infrastructure.Identity;
+
+public static class CustomEndpointsIdentity
+{
+    public static void MapUserEndpoints(this IEndpointRouteBuilder app)
+    {
+        var group = app.MapGroup("/auth").WithTags("Auth");
+
+        group.MapIdentityApi<User>();
+
+        group.MapPost("/logout", async (SignInManager<User> signInManager) =>
+        {
+            await signInManager.SignOutAsync();
+            return Results.Ok(new { message = "Logout realizado com sucesso." });
+        }).RequireAuthorization();
+    }
+}
---
+++ LocadoraVeiculos.Infrastructure/Identity/HiddenEndpointsIdentityFilter.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Infrastructure.Identity
+{
+    public static class HiddenEndpointsIdentityFilter
+    {
+        public static IServiceCollection AddHideEndpointsIdentityFilter(this IServiceCollection services)
+        {
+            services.AddOpenApi(options =>
+            {
+                options.AddDocumentTransformer((document, context, cancellationToken) =>
+                {
+                    var hideRoutes = new[]
+                    {
+                    "/auth/register",
+                    "/auth/refresh",
+                    "/auth/confirmEmail",
+                    "/auth/resendConfirmationEmail",
+                    "/auth/forgotPassword",
+                    "/auth/resetPassword",
+                    "/auth/manage/2fa",
+                };
+
+                    foreach (var path in hideRoutes)
+                    {
+                        document.Paths.Remove(path);
+                    }
+
+                    return Task.CompletedTask;
+                });
+            });
+
+            return services;
+        }
+    }
+}

206a1621b6f4d870aecd0d041c0e24babf6236ab
Felipe <felipecostasiqu@gmail.com>
2026-04-29 18:53:51 -0300

    feat(repository): RepositoryCliente created

--- LocadoraVeiculos.Infrastructure/Repositories/RepositoryCliente.cs
+++ LocadoraVeiculos.Infrastructure/Repositories/RepositoryCliente.cs
@@ -1,13 +1,13 @@
 ﻿using LocadoraVeiculos.Entities.Entities;
 using LocadoraVeiculos.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using LocadoraVeiculos.Application.DTOs;
+using LocadoraVeiculos.Domain.Interfaces.InterfaceCliente;
+using LocadoraVeiculos.Infrastructure.Repositories.Generics;
 
 namespace LocadoraVeiculos.Infrastructure.Repositories
 {
-    public class RepositoryCliente
+    public class RepositoryCliente : RepositoryGenerics<Cliente>, ICliente
     {
         private readonly DbContextOptions<LocadoraContext> _options;
 
@@ -15,20 +15,61 @@ namespace LocadoraVeiculos.Infrastructure.Repositories
         {
             _options = options;
         }
+        
 
-        public async Task<List<Cliente>> ListarClientes(Cliente cliente)
+        public async Task AdicionarCliente(RequestAdicionarClienteDTO clienteDto)
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                Cliente cliente = new Cliente
+                {
+                   Nome = clienteDto.Nome,
+                   CPF = clienteDto.CPF,
+                   Email = clienteDto.Email,
+                   Telefone = clienteDto.Telefone,
+                   DataNasc = clienteDto.DataNasc,
+                   Endereco = clienteDto.Endereco,
+                };
+                
+                await data.Clientes.AddAsync(cliente);
+                
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task EditarCliente(Guid id, RequestEditarClienteDTO clienteDto)
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                var clienteAntigo = await data.Clientes.FindAsync(id);
+                
+                clienteAntigo.Nome = clienteDto.Nome;
+                clienteAntigo.CPF = clienteDto.CPF;
+                clienteAntigo.Email = clienteDto.Email;
+                clienteAntigo.DataNasc = clienteDto.DataNasc;
+                clienteAntigo.Endereco = clienteDto.Endereco;
+                clienteAntigo.Telefone = clienteDto.Telefone;
+                clienteAntigo.Ativo = clienteDto.Ativo;
+                
+                await data.SaveChangesAsync();
+            }
+        }
+
+        public async Task<Cliente> ListarClientePorId(Guid id)
+        {
+            using (var data = new LocadoraContext(_options))
+            {
+                var cliente = await data.Clientes.FindAsync(id);
+
+                return cliente;
+            }
+        }
+
+        public async Task<List<Cliente>> ListarClientes()
         {
             using (var data = new LocadoraContext(_options))
             {
                 return await data.Clientes.AsNoTracking().ToListAsync();
-            }
-        }
-
-        public async Task<Cliente?> ListarClientePorId(Guid id)
-        {
-            using (var data = new LocadoraContext(_options))
-            {
-                return await data.Clientes.FindAsync(id);
             }
         }
     }

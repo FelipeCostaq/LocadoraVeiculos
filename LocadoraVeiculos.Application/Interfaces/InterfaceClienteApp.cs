6895273e0df15b7b753f5484f895b96ab2ac440d
Felipe <felipecostasiqu@gmail.com>
2026-04-29 18:50:10 -0300

    feat(interface): some interfaces created in application layer

---
+++ LocadoraVeiculos.Application/Interfaces/InterfaceClienteApp.cs
@@ -0,0 +1,12 @@
+﻿using LocadoraVeiculos.Application.DTOs;
+using LocadoraVeiculos.Entities.Entities;
+
+namespace LocadoraVeiculos.Application.Interfaces;
+
+public interface InterfaceClienteApp
+{
+    Task AdicionarCliente(RequestAdicionarClienteDTO clienteDto);
+    Task EditarCliente(Guid id, RequestEditarClienteDTO clienteDto);
+    Task<List<Cliente>> ListarClientes();
+    Task<Cliente> ListarClientePorId(Guid id);
+}

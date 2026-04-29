dd281f5e90b834acae453dac54c97a57b82c033f
Felipe <felipecostasiqu@gmail.com>
2026-04-29 18:48:22 -0300

    feat(dto): request dtos created

---
+++ LocadoraVeiculos.Entities/DTOs/RequestAdicionarClienteDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace LocadoraVeiculos.Application.DTOs;
+
+public class RequestAdicionarClienteDTO
+{
+    public string Nome { get; set; } = string.Empty;
+
+    public string CPF { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
+
+    public string Telefone { get; set; } = string.Empty;
+
+    public DateOnly DataNasc { get; set; }
+
+    public string? Endereco { get; set; } = string.Empty;
+}
---
+++ LocadoraVeiculos.Entities/DTOs/RequestEditarClienteDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace LocadoraVeiculos.Application.DTOs;
+
+public class RequestEditarClienteDTO
+{
+    public string Nome { get; set; } = string.Empty;
+
+    public string CPF { get; set; } = string.Empty;
+    
+    public string Email { get; set; } = string.Empty;
+    
+    public string Telefone { get; set; } = string.Empty;
+
+    public DateOnly DataNasc { get; set; }
+
+    public string? Endereco { get; set; } = string.Empty;
+
+    public bool Ativo { get; set; } = true;
+}

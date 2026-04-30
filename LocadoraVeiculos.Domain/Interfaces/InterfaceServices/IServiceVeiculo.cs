5e117a29e2dbe896a5818b8d22c2012d4358262b
Felipe <felipecostasiqu@gmail.com>
2026-04-30 18:40:11 -0300

    feat(interface): interface services created

---
+++ LocadoraVeiculos.Domain/Interfaces/InterfaceServices/IServiceVeiculo.cs
@@ -0,0 +1,10 @@
+﻿using LocadoraVeiculos.Application.DTOs;
+
+namespace LocadoraVeiculos.Domain.Interfaces.InterfaceServices;
+
+public interface IServiceVeiculo
+{
+    public Task<bool> AdicionarVeiculo(RequestAdicionarVeiculoDTO veiculoDto);
+
+    public Task<bool> EditarVeiculo(Guid id, RequestEditarVeiculoDTO veiculoDto);
+}
---
+++ LocadoraVeiculos.Domain/Interfaces/InterfaceServices/IServiceVeiculoAlocado.cs
@@ -0,0 +1,15 @@
+﻿using LocadoraVeiculos.Application.DTOs;
+using LocadoraVeiculos.Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Domain.Interfaces.InterfaceServices
+{
+    public interface IServiceVeiculoAlocado
+    {
+        public Task<bool> AdicionarVeiculoAlocado(RequestAdicionarVeiculoAlocadoDTO veiculoDto);
+
+        public Task<bool> DarBaixaVeiculoAlocado(Guid id);
+    }
+}

c482466991a3d59e695e915e78a4dab24a45c57f
Felipe <felipecostasiqu@gmail.com>
2026-04-30 18:38:48 -0300

    feat(interface): some interfaces created

---
+++ LocadoraVeiculos.Domain/Interfaces/InterfaceVeiculo/IVeiculo.cs
@@ -0,0 +1,15 @@
+﻿using LocadoraVeiculos.Application.DTOs;
+using LocadoraVeiculos.Entities.Entities;
+
+namespace LocadoraVeiculos.Domain.Interfaces.InterfaceVeiculo;
+
+public interface IVeiculo
+{
+    public Task AdicionarVeiculo(RequestAdicionarVeiculoDTO veiculoDto);
+
+    public Task EditarVeiculo(Guid id, RequestEditarVeiculoDTO veiculoDto);
+
+    public Task<List<Veiculo>> ListarVeiculos();
+
+    public Task<List<Veiculo>> ListarVeiculosDisponiveis();
+}
---
+++ LocadoraVeiculos.Domain/Interfaces/InterfaceVeiculoAlocado/IVeiculoAlocado.cs
@@ -0,0 +1,20 @@
+﻿using LocadoraVeiculos.Application.DTOs;
+using LocadoraVeiculos.Entities.DTOs;
+using LocadoraVeiculos.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LocadoraVeiculos.Domain.Interfaces.InterfaceVeiculoAlocado
+{
+    public interface IVeiculoAlocado
+    {
+        public Task AdicionarVeiculoAlocado(RequestAdicionarVeiculoAlocadoDTO veiculoDto);
+
+        public Task DarBaixaVeiculoAlocado(Guid id);
+
+        public Task<List<VeiculoAlocado>> ListarVeiculosAlocados();
+
+        public Task<List<VeiculoAlocado>> ListarVeiculosAlocadosDisponibilidade();
+    }
+}

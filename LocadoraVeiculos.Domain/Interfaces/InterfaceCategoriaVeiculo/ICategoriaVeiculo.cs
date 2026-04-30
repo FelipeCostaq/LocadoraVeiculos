85cc9dd35ed628d8165a1450ff1c69111d1d13eb
Felipe <felipecostasiqu@gmail.com>
2026-04-30 18:41:53 -0300

    feat(app): interfaces

---
+++ LocadoraVeiculos.Domain/Interfaces/InterfaceCategoriaVeiculo/ICategoriaVeiculo.cs
@@ -0,0 +1,14 @@
+﻿using LocadoraVeiculos.Application.DTOs;
+using LocadoraVeiculos.Domain.Interfaces.Generics;
+using LocadoraVeiculos.Entities.Entities;
+
+namespace LocadoraVeiculos.Domain.Interfaces.InterfaceCategoriaVeiculo;
+
+public interface ICategoriaVeiculo : IGenerics<CategoriaVeiculo>
+{
+    public Task AdicionarCategoriaVeiculo(RequestAdicionarCategoriaVeiculoDTO categoriaVeiculoDto);
+
+    public Task EditarCategoriaVeiculo(Guid id, RequestEditarCategoriaVeiculoDTO categoriaVeiculoDto);
+
+    public Task<List<CategoriaVeiculo>> ListarCategoriasVeiculo();
+}
---
+++ LocadoraVeiculos.Domain/Interfaces/InterfaceServices/IServiceCategoriaVeiculo.cs
@@ -0,0 +1,10 @@
+﻿using LocadoraVeiculos.Application.DTOs;
+
+namespace LocadoraVeiculos.Domain.Interfaces.InterfaceServices;
+
+public interface IServiceCategoriaVeiculo
+{
+    public Task<bool> AdicionarCategoriaVeiculo(RequestAdicionarCategoriaVeiculoDTO categoriaVeiculoDto);
+
+    public Task<bool> EditarCategoriaVeiculo(Guid id, RequestEditarCategoriaVeiculoDTO categoriaVeiculoDto);
+}

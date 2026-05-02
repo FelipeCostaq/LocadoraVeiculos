2d26564fa6c91e3e4c4fb176aa2e0f231598d283
FelipeCostaq <felipecostasiqu@gmail.com>
2026-05-02 13:54:36 -0300

    feat: blob storage logic

--- LocadoraVeiculos.Infrastructure/AzureBlobStorage/AzureBlobStorageService.cs
+++ LocadoraVeiculos.Infrastructure/AzureBlobStorage/AzureBlobStorageService.cs
@@ -1,6 +1,32 @@
-﻿namespace LocadoraVeiculos.Infrastructure.AzureBlobStorage;
+﻿using Azure.Storage.Blobs;
+using LocadoraVeiculos.Domain.Interfaces.InterfaceServices;
+using Microsoft.Extensions.Configuration;
 
-public class AzureBlobStorageService
+namespace LocadoraVeiculos.Infrastructure.AzureBlobStorage;
+
+public class AzureBlobStorageService  : IServiceStorage
 {
+    private readonly string _connectionString;
+    private readonly string _containerName;
     
+    public AzureBlobStorageService(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetValue<string>("AzureStorage:ConnectionString");
+        _containerName = configuration.GetValue<string>("AzureStorage:ContainerName");
+    }
+
+    public async Task<string> UploadFileAsync(Stream fileStream, string fileName)
+    {
+        var blobServiceClient = new BlobServiceClient(_connectionString);
+        var containerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+        
+        await containerClient.CreateIfNotExistsAsync();
+        
+        var novoNome = $"{Guid.NewGuid()}-{fileName}";
+        var blobClient = containerClient.GetBlobClient(novoNome);
+        
+        await blobClient.UploadAsync(fileStream);
+        
+        return blobClient.Uri.ToString();
+    }
 }
